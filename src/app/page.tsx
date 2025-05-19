4da7317b48fa077278b9813e2c2354ea15c2fba4
yu.wang <wangyu@wycode.cn>
2025-05-19 13:48:35 +0800

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,87 @@
+"use client";
+import { AppstoreAddOutlined, MoonOutlined, ProductOutlined, SunOutlined } from "@ant-design/icons";
+import { Prompts, Sender, Welcome } from "@ant-design/x";
+import { Button, ConfigProvider, theme } from "antd";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { App } from 'antd';
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const { message } = App.useApp();
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const theme = localStorage.getItem('theme');
+      if (theme) {
+        setIsDarkMode(theme === 'dark');
+      } else {
+        setIsDarkMode(window.matchMedia('(prefers-color-scheme: dark)').matches);
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }, [isDarkMode])
+
+  const prompts = [
+    {
+      key: '1',
+      icon: <ProductOutlined style={{ color: '#FFD700' }} />,
+      label: '获取应用列表',
+      description: '获取所有正在运行的应用列表',
+    },
+    {
+      key: '2',
+      icon: <AppstoreAddOutlined style={{ color: '#1890FF' }} />,
+      label: '构建一个用户管理系统',
+      description: '请帮我构建一个用户管理系统',
+    }
+  ];
+
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
+      }} >
+      <main className="flex  flex-col min-h-screen items-center bg-[url(./img/bg.svg)] bg-cover bg-center">
+        <Button className="self-end m-8" type="text" icon={isDarkMode ? <MoonOutlined /> : <SunOutlined />} onClick={toggleTheme} />
+        <div className="container flex flex-col flex-1 gap-4 h-full items-center">
+          <div className="flex-1 flex flex-col justify-center gap-8">
+            <Welcome
+              icon={<Image src="/logo.png" alt="icon" width={58} height={58} priority />}
+              title="欢迎访问哈巴狗"
+              description="今天准备做个什么呢？"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <Prompts
+              title="🤔 你可能想要:"
+              items={prompts}
+              onItemClick={(info) => {
+                message.success(`You clicked a prompt: ${info.data.label}`);
+              }}
+            />
+          </div>
+          <Sender className="bg-white dark:bg-black" />
         </div>
+        <footer className="flex flex-col items-center justify-center p-4 text-xs text-gray-800 dark:text-gray-200 gap-2">
+          <p>© 2025 哈巴狗. All rights reserved.</p>
+          <p className="flex text-sky-600 gap-4">
+            <a href="https://beian.miit.gov.cn/" target="_blank">陕ICP备2025063359号-1</a>
+            <a className="flex gap-1 items-center" href="https://beian.mps.gov.cn/#/query/webSearch?code=61011302002082" rel="noreferrer" target="_blank"><Image src="/police.png" alt="公安备案图标" width={16} height={16} />陕公网安备61011302002082号</a>
+          </p>
+        </footer>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </ConfigProvider>
   );
 }
